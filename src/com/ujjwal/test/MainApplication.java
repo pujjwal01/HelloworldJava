d23b19ad0a943ae620349de4ad7b176ad52b81d2
ujjwalpandey <ujjwalpandey@ujjwals-air.lan>
2022-07-13 09:49:53 -0500

    Initial commit

---
+++ src/com/ujjwal/test/MainApplication.java
@@ -0,0 +1,11 @@
+package com.ujjwal.test;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello world");
+	}
+
+}
